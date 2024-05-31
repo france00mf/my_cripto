d4628cf06c1b2f64c3d350734315f4c53122c5bb
France! <franciscodomingosjose06798@gmail.com>
2024-05-31 20:37:28 +0100

    finesh stablishSocketconnection method

--- lib/core/data/remote/binance/binance_service.dart
+++ lib/core/data/remote/binance/binance_service.dart
@@ -1,4 +1,6 @@
 
+import 'dart:convert';
+
 import 'package:my_cripto/core/data/remote/binance/binance_interface_imp.dart';
 import 'package:my_cripto/utils/app_logger.dart';
 import 'package:web_socket_channel/src/channel.dart';
@@ -8,8 +10,23 @@ class BinanceServiceImpl implements BinanceService{
   final _logger= appLogger(BinanceServiceImpl);
   @override
   WebSocketChannel establishSocketConnection({required String symbol, required String interval}) {
-      final channer=WebSocketChannel.connect(Uri.parse('wss://stream.binance.com:9443/ws'));
-throw UnimplementedError();
+      final channel=WebSocketChannel.connect(Uri.parse('wss://stream.binance.com:9443/ws'));
+
+      channel.sink.add(jsonEncode({
+            'method': 'SUBSCRIBE',
+          'params': ['$symbol@kline_$interval'],
+          'id': 1
+      }));
+
+      channel.sink.add(
+        jsonEncode({
+          'method': 'SUBSCRIBE',
+          'params': ['$symbol@depth'],
+          'id': 1
+        })
+      );
+
+ return channel;
       
     
   }
