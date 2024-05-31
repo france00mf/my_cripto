ddfb9d393cf88ead9e91df35b3f55bbf4a188ef5
France! <franciscodomingosjose06798@gmail.com>
2024-05-31 17:10:42 +0100

    add binance interface and websocket_channel pack

---
+++ lib/core/data/remote/binance/binance_interface.dart
@@ -0,0 +1,21 @@
+
+
+import 'package:web_socket_channel/web_socket_channel.dart';
+
+abstract class BinanceInterface {
+  
+  Future<List<void>> getCandles({
+    required String symbol,
+    required String interval,
+    int? endTime,
+  });
+
+  WebSocketChannel establishSocketConnection({
+    required String symbol,
+    required String interval,
+  });
+}
+
+abstract class BinanceService implements BinanceInterface {}
+
+abstract class BinanceRepository implements BinanceInterface {}
