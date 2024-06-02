809107253aa21757e4bab7c8422c53e49da27cc9
France! <franciscodomingosjose06798@gmail.com>
2024-06-02 08:26:54 +0100

    fix Erros on getSymbols method

--- lib/core/data/model/symbol_response_model.dart
+++ lib/core/data/model/symbol_response_model.dart
@@ -1,6 +1,14 @@
 
+import 'dart:convert';
+
 class SymbolResponseModel{
   final String symbol;
   final String price;
   SymbolResponseModel( {required this.symbol,required this.price,});
+
+  factory SymbolResponseModel.fromJson(String source)=> SymbolResponseModel.fromMap(json.decode(source) as Map <String, dynamic>);
+
+  factory SymbolResponseModel.fromMap(Map<String, dynamic> map){
+    return SymbolResponseModel(symbol: map["symbol"] as String, price: map["price"] as String);
+  }
 }
