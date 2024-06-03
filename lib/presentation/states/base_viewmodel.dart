6e577fdac8e4b4f256373c51c34b207ca134ec83
France! <franciscodomingosjose06798@gmail.com>
2024-06-03 16:26:38 +0100

    fixErrors on some files (change code)

--- lib/presentation/states/base_viewmodel.dart
+++ lib/presentation/states/base_viewmodel.dart
@@ -1,10 +1,7 @@
-import 'package:candlesticks/candlesticks.dart';
-import 'package:flutter/foundation.dart';
-import 'package:my_cripto/core/data/model/candle_stick_ticker_model.dart';
-import 'package:my_cripto/core/data/model/order_book_model.dart';
-import 'package:my_cripto/core/data/model/symbol_response_model.dart';
+
+
+import 'package:flutter/material.dart';
 import 'package:my_cripto/utils/app_logger.dart';
-import 'package:web_socket_channel/web_socket_channel.dart';
 
 import 'viewmodel_state.dart';
 
@@ -12,64 +9,5 @@ class BaseViewModel extends ChangeNotifier {
   late final _logger = appLogger(BaseViewModel);
   AppLogger get logger => _logger;
 
-    int _selectedInterval = 0;
-  int get selectedInterval => _selectedInterval;
-  List<SymbolResponseModel> _symbols = [];
-  List<SymbolResponseModel> get symbols => _symbols;
-  WebSocketChannel? _channel;
-  WebSocketChannel? get channel => _channel;
-
-
-    List<Candle> _candles = [];
-  List<Candle> get candles => _candles;
-  String _currentInterval = "1H";
-  String get currentInterval => _currentInterval;
-  SymbolResponseModel? _currentSymbol;
-  SymbolResponseModel? get currentSymbol => _currentSymbol;
-  int _currentTabIndex = 0;
-  int get currentTabIndex => _currentTabIndex;
-  int _bottomTabIndex = 0;
-  int get bottomTabIndex => _bottomTabIndex;
-
-    CandleTickerModel? _candleTicker;
-  CandleTickerModel? get candleTicker => _candleTicker;
-  OrderBook? _orderBooks;
-  OrderBook? get orderBooks => _orderBooks;
-
-
-   setBottomTabIndex(int index) {
-    _bottomTabIndex = index;
-    notifyListeners();
-  }
-
-  setTabIndex(int index) {
-    _currentTabIndex = index;
-    notifyListeners();
-  }
-
-  setInterval(String interval) {
-    _currentInterval = interval;
-    notifyListeners();
-  }
-
-    setCurrentSymbol(SymbolResponseModel v) {
-    _currentSymbol = v;
-    notifyListeners();
-  }
-
-   setSelectedInterval(int index) {
-    _selectedInterval = index;
-    notifyListeners();
-  }
-
-  setCandleTicker(CandleTickerModel candleT) {
-    _candleTicker = candleT;
-    notifyListeners();
-  }
-
-  setOrderBook(OrderBook data) {
-    _orderBooks = data;
-    notifyListeners();
-  }
 
 }
