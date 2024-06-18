16433ef82414aad517d96572d4b9bd4ad10e3cd8
France! <franciscodomingosjose06798@gmail.com>
2024-06-18 12:16:21 +0100

    add button on timeFrame

--- lib/presentation/views/home/components/time_frame_section.dart
+++ lib/presentation/views/home/components/time_frame_section.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:my_cripto/extensions/context_extension.dart';
 import 'package:my_cripto/presentation/views/home/viewmodels/home_viewmodel.dart';
+import 'package:my_cripto/utils/app_colors.dart';
+import 'package:my_cripto/widgets/app_text.dart';
+import 'package:my_cripto/widgets/gap.dart';
 
 class TimerFrameSection extends ConsumerWidget{
 
@@ -23,7 +28,47 @@ class TimerFrameSection extends ConsumerWidget{
     final vm = ref.watch(homeViewModelProvider);
 
     return Padding(padding: const EdgeInsets.only(left: 16),
-    child: Container(),
+    child: SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+      children: [
+        AppText.body1('Time',
+        color: context.isDarkMode ? AppColors.white : AppColors.blackTint2,
+        ),
+        Gap.w4,
+        ...timeframes.map((e) => 
+        InkWell(
+          onTap: (){
+            onSelected.call(e);
+            vm.setInterval(e);
+          },
+          child: AnimatedContainer(
+            duration: const Duration(milliseconds: 400),
+            width: 40,
+            margin: const EdgeInsets.symmetric(horizontal: 3),
+            padding: const EdgeInsets.symmetric(
+              vertical: 6
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(100),
+              color: vm.currentInterval == e ? context.isDarkMode  ? const Color(0xff555C63)
+                            : const Color(0xffCFD3D8)
+                        : Colors.transparent,
+            ),
+            child: Center(
+              child: AppText.body1(
+                e, 
+            
+                      color: context.isDarkMode
+                          ? AppColors.white
+                          : AppColors.blackTint2,
+              ),
+            ),
+          ),
+        )
+        )
+      ],
+    )),
     );
   }
 
